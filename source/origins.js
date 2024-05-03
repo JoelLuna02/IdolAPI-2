6c346568d7e942d4ccfb42775ea8cdf670630e4e
JoelLuna02 <hectorjoelluna@outlook.com.ar>
2024-05-03 00:21:47 -0300

    Add origins (beta 8)

---
+++ source/origins.js
@@ -0,0 +1,22 @@
+import cors from "cors";
+
+/**
+ * This feature allows CORS policies to be applied to certain web domains.
+ * @param {import("express").Express} app Express application instance
+ * @param {string[]} origins List of allowed websites
+ * @returns {Promise<void>}
+ */
+export function AllowCORS(app, origins) {
+	app.use(
+		cors({
+			origin: function (origin, callback) {
+				if (!origin) return callback(null, true);
+				if (origins.indexOf(origin) === -1) {
+					let msg = "Access to the Origin is not permitted on this site.";
+					return callback(new Error(msg), false);
+				}
+				return callback(null, true);
+			},
+		}),
+	);
+}
